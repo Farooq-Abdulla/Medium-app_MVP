7d315bca8592e87a4ce01d853485f5766d4d0249
Farooq Abdulla <farooqabdulla279@gmail.com>
2024-04-15 22:17:38 -0500

    changed config.ts in frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL= 'https://backend.farooqabdulla279.workers.dev'
-export const BACKEND_URL= 'http://localhost:8787'
+export const BACKEND_URL= 'https://backend.farooqabdulla279.workers.dev'
